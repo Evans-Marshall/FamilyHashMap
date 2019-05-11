040e73f802c4c36229803ce3837f333c9abffe56
Marshall <32920499+YungMoose@users.noreply.github.com>
2019-05-10 22:57:57 -0600

    First commit, uploading all files

---
+++ Collections Familiarity/src/arrayListFamily/FamilyList.java
@@ -0,0 +1,32 @@
+package arrayListFamily;
+import java.util.HashMap;
+import java.util.Map;
+
+public class FamilyList {
+    public static void main(String[] args) {
+        
+
+    	HashMap<String, String> familyMap = new HashMap<String, String>();
+        runMap(familyMap);
+    }
+    
+    	public static void runMap (Map<String, String> familyMap) {
+    	familyMap.put("Father", "Marshall");
+        familyMap.put("Mother", "Raena");
+        familyMap.put("First Child", "Tyler");
+        familyMap.put("Second Child", "Tanner");
+        familyMap.put("Third Child", "RobbieJo");
+        familyMap.put("Fourth Child", "Michael");
+        familyMap.put("Fifth Child", "Allie");
+        familyMap.put("Sixth Child", "Charly");
+        familyMap.put("Seventh Child", "Benjamin");
+        
+        
+        for (String key: familyMap.keySet()) {
+        	String value = familyMap.get(key);
+            System.out.println(key + " is " + value);
+        }
+    
+
+}
+}
